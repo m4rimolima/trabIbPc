78f20ed5786a1782e7fcb6cdbec8818e5820eebb
Mari🦦 <168196980+m4rimolima@users.noreply.github.com>
2024-06-06 22:41:14 -0300

    Update q08 to show all the operations at once

--- questao08.py
+++ questao08.py
@@ -1,14 +1,6 @@
 n1 = int(input("Me diga um número"))
-operacao = input("Digite uma operação")
 n2 = int(input("Me diga outro número"))
-if operacao == '+':
-    resultado = n1 + n2
-elif operacao == '-':
-    resultado = n1 - n2
-elif operacao == '/':
-    resultado = n1 / n2
-elif operacao == '*':
-    resultado = n1 * n2
-else:
-    resultado = "Operação não reconhecida"
-print(f'{resultado} é o resultado da sua operação')
+print(f'{n1}x{n2} = {n1*n2}')
+print(f'{n1}%{n2} = {n1/n2}')
+print(f'{n1}+{n2} = {n1+n2}')
+print(f'{n1}-{n2} = {n1-n2}')
