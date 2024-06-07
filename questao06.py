4c45a6c4e99496f8063e74b73f9fcc8ddf3de302
Mari🦦 <168196980+m4rimolima@users.noreply.github.com>
2024-06-06 22:27:46 -0300

    Update q06 adding the age

--- questao06.py
+++ questao06.py
@@ -1,6 +1,6 @@
 nome=input("Me diga o seu nome")
 idade=int(input("Me diga sua idade"))
 if idade>=16:
-     print(f'{nome} você pode emitir seu título de eleitor')
+     print(f'{nome}, você tem {idade} anos e por isso  você pode emitir seu título de eleitor')
 else:
-    print(f'{nome} você não pode emitir seu título de eleitor')
+    print(f'{nome}, você tem {idade} anos e por isso  você não pode emitir seu título de eleitor')
