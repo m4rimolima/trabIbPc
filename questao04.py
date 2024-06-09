6383d4f64349f274cbf121ffb0ee96e66afe381d
Mari🦦 <168196980+m4rimolima@users.noreply.github.com>
2024-06-09 20:23:11 -0300

    Update q04 to tell me letter or  characters

--- questao04.py
+++ questao04.py
@@ -1,3 +1,3 @@
-Frase=input("O que você mais gosta de fazer?")
+Frase=input("Me diga uma frase para saber qual sua última letra ou caractere")
 Frase_resp= Frase[-1]
-print (f'A última letra é {Frase_resp}')
+print (f'A última letra ou caractere é {Frase_resp}')
